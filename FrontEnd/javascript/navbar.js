9cd4efbfe96aa00d0e5cc93f424f76c5e2e31fb1
DatDuc <datducnguyenhuu@gmail.com>
2018-04-25 23:07:40 +0700

    client - printTshirt

--- FrontEnd/javascript/navbar.js
+++ FrontEnd/javascript/navbar.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
-    window.onscroll = function() { myFunction(); };
-
     var navbar = document.getElementById("navbar");
-
-    function myFunction() {
-        if (window.pageYOffset >= 200) {
-            navbar.classList.add("sticky");
-        } else {
-            navbar.classList.remove("sticky");
+    $(window).scroll(function() {
+            if ($(this).scrollTop() > 150) {
+                navbar.classList.add("sticky");
+            } else {
+                navbar.classList.remove("sticky");
+            }
         }
-    }
 
-
+    );
 });
