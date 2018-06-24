407d3540523e5c1826626dd1dea07b10f3c5375e
DatDuc <datducnguyenhuu@gmail.com>
2018-06-24 23:53:08 +0700

    Update UI client site

--- Web/routes/client.js
+++ Web/routes/client.js
@@ -7,5 +7,13 @@ router.get('/', (req, res)=>{
     res.render('client/client', {layout: "layoutClient"})
 });
 
+router.get('/shop', (req, res)=>{
+	res.render('client/shop', {layout: "layoutClient"});
+});
+
+
+router.get('/design', (req, res)=>{
+	res.render('client/design', {layout: "layoutClient"});
+});
 
 module.exports = router;
